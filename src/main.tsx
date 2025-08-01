f2ffa6899a5928a25b2b3f89bbab72ae8dfba468
cristian.mendes <cristian.mendes@lideratacadista.com.br>
2025-08-01 18:43:29 -0300

    Adicionado pacotes de analises do vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {AppRoutes} from "./assets/context/AppRoutes.tsx";
-import {createTheme, CssBaseline,  ThemeProvider} from "@mui/material";
+import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+import {SpeedInsights} from "@vercel/speed-insights/next"
+import {Analytics} from "@vercel/analytics/next"
 
 const darkTheme = createTheme({
     palette: {
@@ -11,6 +13,8 @@ const darkTheme = createTheme({
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
+        <SpeedInsights/>
+        <Analytics/>
         <ThemeProvider theme={darkTheme}>
             <CssBaseline/>
             <AppRoutes/>
