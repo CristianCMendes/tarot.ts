329de729e4f76f6e14a66af646c832f753704646
cristian.mendes <cristian.mendes@lideratacadista.com.br>
2025-08-01 18:45:59 -0300

    ops, tinha colocado a versão next.js inves de react, corregido

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {AppRoutes} from "./assets/context/AppRoutes.tsx";
 import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
-import {SpeedInsights} from "@vercel/speed-insights/next"
-import {Analytics} from "@vercel/analytics/next"
+import {SpeedInsights} from "@vercel/speed-insights/react"
+import {Analytics} from "@vercel/analytics/react"
 
 const darkTheme = createTheme({
     palette: {
