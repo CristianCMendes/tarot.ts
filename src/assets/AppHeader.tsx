8ec4711007e44a00e487bc2dd4bdf976f6040ed1
Cristian C Mendes <dontetb288@gmail.com>
2026-08-31 22:34:42 -0300

    Incrementada versão para 1.0.6 e ajustados estilos no componente `AppHeader` para melhor legibilidade.

--- src/assets/AppHeader.tsx
+++ src/assets/AppHeader.tsx
@@ -2,24 +2,24 @@ import {AppBar, Grid, IconButton, Typography} from "@mui/material";
 import {GitHub, LinkedIn} from "@mui/icons-material";
 
 export function AppHeader() {
-    return (<AppBar sx={{p: 1, px: 2}}>
-        <Grid container justifyContent={'space-between'} alignItems={'center'}>
-            <Grid>
-                <Typography>Tarot by Cristian C Mendes</Typography>
-            </Grid>
-            <Grid container>
-                <Grid>
-                    <IconButton
-                        onClick={() => window.open('https://www.linkedin.com/in/cristian-cardoso-mendes-b29a5216b/', '_blank')}>
-                        <LinkedIn/>
-                    </IconButton>
-                </Grid>
-                <Grid>
-                    <IconButton onClick={() => window.open('https://github.com/CristianCMendes/tarot.ts', '_blank')}>
-                        <GitHub/>
-                    </IconButton>
-                </Grid>
-            </Grid>
-        </Grid>
-    </AppBar>)
+	return (<AppBar sx={{p: 1, px: 2}}>
+		<Grid container sx={{justifyContent: 'space-between', alignItems: 'center'}}>
+			<Grid>
+				<Typography>Tarot by Cristian C Mendes</Typography>
+			</Grid>
+			<Grid container>
+				<Grid>
+					<IconButton
+						onClick={() => window.open('https://www.linkedin.com/in/cristian-cardoso-mendes-b29a5216b/', '_blank')}>
+						<LinkedIn/>
+					</IconButton>
+				</Grid>
+				<Grid>
+					<IconButton onClick={() => window.open('https://github.com/CristianCMendes/tarot.ts', '_blank')}>
+						<GitHub/>
+					</IconButton>
+				</Grid>
+			</Grid>
+		</Grid>
+	</AppBar>)
 }
